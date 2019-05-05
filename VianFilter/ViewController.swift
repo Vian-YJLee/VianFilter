252464ff4e4f5d85597abd4ca1edd3c3d7f39cb7
Vian-YJLee <Vianney.yjlee@gmail.com>
2019-05-05 11:43:12 +0900

    first Commit

--- VianFilter/ViewController.swift
+++ VianFilter/ViewController.swift
@@ -7,14 +7,54 @@
 //
 
 import UIKit
+import MobileCoreServices
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
+    @IBOutlet var imgView: UIImageView!
+    
+    let imagePicker: UIImagePickerController! = UIImagePickerController()
+    var captureImage: UIImage!
+    var videoURL: URL!
+    var flagImageSave = false
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
 
+    @IBAction func btnCaptureImageFromCamera(_ sender: UIButton) {
+        
+        if (UIImagePickerController.isSourceTypeAvailable(.camera)) {
+            flagImageSave = true
+            
+            imagePicker.delegate = self
+            imagePicker.sourceType = .camera
+            imagePicker.mediaTypes = [kUTTypePNG as String]
+            imagePicker.allowsEditing = true
+            
+            present(imagePicker, animated: true, completion: nil)
+        }
+    }
+    @IBAction func btnLoadImageFromLibrary(_ sender: UIButton) {
+    }
+    
+    
+    
+    @IBAction func btnRecordVideoFromCamera(_ sender: UIButton) {
+    }
+    @IBAction func btnLoadVideoFromLibrary(_ sender: UIButton) {
+    }
+    
+    func myAlert(_ title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+        let action =  UIAlertAction(title: "확인", style: UIAlertAction.Style.default, handler: nil)
+        alert.addAction(action)
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    
 }
 
---
+++ pods/Target Support Files/Pods-VianFilter/Pods-VianFilter-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_VianFilter : NSObject
+@end
+@implementation PodsDummy_Pods_VianFilter
+@end
