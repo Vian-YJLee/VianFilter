34d22381a7d9d104ad03c0a72714fb87b02832ae
Vian-YJLee <Vianney.yjlee@gmail.com>
2019-05-06 16:14:50 +0900

    "기본 동작 테스트. 카메라, 음성, Photo Library 저장 및불러오기구현. Image View에 미디어 올렸을때 앱 정지되는 문제해결해야함"

--- VianFilter/ViewController.swift
+++ VianFilter/ViewController.swift
@@ -32,28 +32,106 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
             
             imagePicker.delegate = self
             imagePicker.sourceType = .camera
-            imagePicker.mediaTypes = [kUTTypePNG as String]
-            imagePicker.allowsEditing = true
+            imagePicker.mediaTypes = [kUTTypeImage as String, kUTTypeMovie as String]
+            imagePicker.allowsEditing = false
+            
+            //카메라 기능
             
             present(imagePicker, animated: true, completion: nil)
         }
     }
+    
     @IBAction func btnLoadImageFromLibrary(_ sender: UIButton) {
+        
+        if (UIImagePickerController.isSourceTypeAvailable(.photoLibrary)) {
+            flagImageSave = false
+            
+            imagePicker.delegate = self
+            imagePicker.sourceType = .photoLibrary
+            imagePicker.mediaTypes = [kUTTypeImage as String]
+            imagePicker.allowsEditing = true
+            
+            present(imagePicker, animated: true, completion: nil)
+        }
+        //사진 형식 파일만 추려서 불러오기, 편집허용
     }
-    
-    
     
     @IBAction func btnRecordVideoFromCamera(_ sender: UIButton) {
+        
+        if (UIImagePickerController.isSourceTypeAvailable(.camera)) {
+            flagImageSave = true
+            
+            imagePicker.delegate = self
+            imagePicker.sourceType = .camera
+            imagePicker.mediaTypes = [kUTTypeMovie as String]
+            imagePicker.allowsEditing = true
+            
+            present(imagePicker, animated: true, completion: nil)
+        }
+        //비디오 사용
     }
     @IBAction func btnLoadVideoFromLibrary(_ sender: UIButton) {
+        
+        if (UIImagePickerController.isSourceTypeAvailable(.photoLibrary)) {
+            flagImageSave = false
+            
+            imagePicker.delegate = self
+            imagePicker.sourceType = .photoLibrary
+            imagePicker.mediaTypes = [kUTTypeMovie as String]
+            imagePicker.allowsEditing = false
+            
+            present(imagePicker, animated: true, completion: nil)
+        }
+        
+        //비디오만 추려서 불러오기
     }
+    //선택, 촬영된 미디어의 타입 확인 후 뷰에 띄우는 Delegate Method
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let mediaType = info[UIImagePickerController.InfoKey.mediaType] as! NSString
+        
+        if mediaType.isEqual(to: kUTTypeImage as NSString as String) {
+           
+            captureImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
+            
+            if flagImageSave {
+                
+                UIImageWriteToSavedPhotosAlbum(captureImage, self, nil, nil)
+                //flagImageSave가 true인 경우 사진을 앨범에 저장
+            }
+            
+            imgView.image = captureImage //이미지뷰에 출력
+        }
+        
+        else if mediaType.isEqual(to: kUTTypeMovie as NSString as String) {
+            
+            if flagImageSave {
+                
+                videoURL = (info[UIImagePickerController.InfoKey.mediaURL] as! URL)
+                
+                UISaveVideoAtPathToSavedPhotosAlbum(videoURL.relativePath, self, nil, nil)
+            }
+        }
+    }
+    
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        self.dismiss(animated: true, completion: nil)
+        
+    }
+    
+    
     
     func myAlert(_ title: String, message: String) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
         let action =  UIAlertAction(title: "확인", style: UIAlertAction.Style.default, handler: nil)
         alert.addAction(action)
         self.present(alert, animated: true, completion: nil)
+        //동작 오류 경고
     }
+    
+    
+    
     
     
 }
